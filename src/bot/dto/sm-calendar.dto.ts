71d2f0b24ab13ee532efc5f2dfd0a401a1f09382
uwriym <rim2wo@gmail.com>
2025-02-20 18:58:53 +0900

    sm-calendar.dto.ts 수정
    - scraperName 속성 제거

--- src/bot/dto/sm-calendar.dto.ts
+++ src/bot/dto/sm-calendar.dto.ts
@@ -2,9 +2,6 @@ import {IsString} from 'class-validator'
 
 export class SmCalendarDto {
     @IsString()
-    readonly scraperName!: string
-
-    @IsString()
     readonly title!: string
 
     @IsString()
